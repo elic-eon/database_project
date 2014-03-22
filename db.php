d9489de497f03b24d345858f6bc07eab6bf01160
Elic <elic.space.cs@gmail.com>
2014-03-23 03:30:43 +0800

    remove tracing info

--- db.php
+++ db.php
@@ -3,9 +3,7 @@ require_once('./config.php');
 $dsn = "mysql:host=$DB_HOST;dbname=$DB_NAME";
 try {
 	$db = new PDO($dsn, "$DB_USER", "$DB_PASSWORD");
-	echo "success";
 } catch(PDOException $e) {
 	die('Could not connect to the database:<br/>' . $e);
-	echo "fail";
 }
 ?>
