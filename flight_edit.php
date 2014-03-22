26a92e79ad456bd8a03d81fe5ead29020990bcf4
Elic <elic.space.cs@gmail.com>
2014-03-22 19:50:44 +0800

    applied bootstrap on flight_edit

--- flight_edit.php
+++ flight_edit.php
@@ -12,16 +12,47 @@ else:
 	require_once('layout/header.php');
 	require_once('layout/msg.php');
 ?>
-	<?php require_once('layout/msg.php') ?>
-	<form action="flight_edit_func.php" method="post" role="form">
-		<input name="id" type="hidden" value="<?php echo $result->id ?>">
-		<p>Flight Number: <input name="flightNumber" type="text" value="<?php echo $result->flight_number ?>"></p>
-		<p>Departure: <input name="departure" type="text" value="<?php echo $result->departure ?>"></p>
-		<p>Destination: <input name="destination" type="text" value="<?php echo $result->destination ?>"></p>
-		<p>Departure Date: <input name="departureDate" type="datetime-local" value="<?php echo strftime('%Y-%m-%dT%H:%M:%S', strtotime($result->departure_date)) ?>"></p>
-		<p>Arrival Date: <input name="arrivalDate" type="datetime-local" value="<?php echo strftime('%Y-%m-%dT%H:%M:%S', strtotime($result->arrival_date)) ?>"></p>
-		<p><input type="submit" value="Update"> | <a href="flight.php">Cancel</a></p>
-	</form>
+  <div class="col-md-6">
+    <?php require_once('layout/msg.php') ?>
+    <form action="flight_edit_func.php" method="post" role="form">
+      <div class="form-group">
+        <input name="id" type="hidden" value="<?php echo $result->id ?>">
+      </div>
+      <div class="form-group">
+        <label class="col-sm-4 control-label">Flight Number</label>
+        <div class="col-sm-8">
+          <input name="flightNumber" type="text" class="form-control" value="<?php echo $result->flight_number ?>"></p>
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-4 control-label">Departure</label>
+        <div class="col-sm-8">
+          <input name="departure" type="text" class="form-control" value="<?php echo $result->departure ?>"></p>
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-4 control-label">Destination</label>
+        <div class="col-sm-8">
+          <input name="destination" type="text" class="form-control" value="<?php echo $result->destination ?>"></p>
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-4 control-label">Departure Date</label>
+        <div class="col-sm-8">
+          <input name="departureDate" type="datetime-local" class="form-control" value="<?php echo strftime('%Y-%m-%dT%H:%M:%S', strtotime($result->departure_date)) ?>"></p>
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-4 control-label">Arrival Date</label>
+        <div class="col-sm-8">
+          <input name="arrivalDate" type="datetime-local" class = "form-control" value="<?php echo strftime('%Y-%m-%dT%H:%M:%S', strtotime($result->arrival_date)) ?>"></p>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-sm-offset-4 col-sm-8"><input type="submit" class="btn btn-default"value="Update">   |   <a href="flight.php">Cancel</a></div>
+      </div>
+    </form>
+  </div>
 <?php
 	require_once('layout/footer.php');
 endif;
